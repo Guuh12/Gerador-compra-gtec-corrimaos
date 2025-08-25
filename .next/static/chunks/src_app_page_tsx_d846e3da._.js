b18bf51bba86962a32898c881e36e82bec9f673f
Guuh12 <gustavoyuri34@gmail.com>
2025-08-25 17:34:58 -0300

    Configuração inicial do projeto com logo Gtec Corrimãos

--- .next/static/chunks/src_app_page_tsx_d846e3da._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_4de89458._.js",
-  "static/chunks/node_modules_07b19a71._.js"
-],
-    source: "dynamic"
-});
